bee1a707ff595a1f9dd5499e7a6ff8d8779aa1c3
Jana Valášková <jancav@seznam.cz>
2021-06-06 17:53:55 +0200

    data-atribut--final

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+const obrazky = document.querySelectorAll('img');
+
+obrazky.forEach((obrazek) => {
+	obrazek.addEventListener('click', (udalost) => {
+		//console.log('Ahoj ' + udalost.target.innerText);
+        document.querySelector("#vysledek").innerHTML = udalost.target.dataset.kalorie;
+	});
+});
